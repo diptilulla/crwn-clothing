9e68a5788185257370c67a5c6c6f1cba02bb59ff
diptilulla <60360333+diptilulla@users.noreply.github.com>
2021-05-03 01:28:26 +0530

    updating app with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-
+import { selectCartItemsCount } from '../../redux/cart/cart.selector';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden()) 
 });
-
-export default connect(null,mapDispatchToProps)(CartIcon);
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state) //passing whole reducer state into selectCartItems selector, which references selectCartItems, which ref selectCart, it passes state in there gets cart object, which is passed into selectCartItems, which passes out cartItems which passed to selectCartItemsCount, which finally gives count
+});
+//mapStateToProps is always being called whenever redux state changes i.e new obj returned for eg even while signing in this mapStateToProps is being called unnecessarily, so reduce gets called every time, so we want to moodify code such that component doesn't rerender everytime state changes especially if those state change dont modify parts of state that component doesn't care abt
+//store or cache value what selector(code gets state, pulls off small portion of the state) is using to compute the value - memoization - so that component doesn't rerender - achived by using library reselect, allows ro write selectors in a way suh that they know that if prop that they are pulling from state are same value hasnt changed output is also same it won't pass them i.e. will just pass the old value
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon); //we don't need to use state as props here, we just need to dispatch
---
+++ src/redux/cart/cart.selector.js
@@ -0,0 +1,20 @@
+import { createSelector } from "reselect"; //to cache and now we an reuse this selector
+//2 types of selectors - input selectors - don't use createSelector, & output selectors - use input selecto and creatSelector
+//input selector - func which takes whole state and just returns a lice of it - 1 layer deep usually
+const selectCart = (state) => state.cart; //we want cart reducer out of root reducer/state
+
+export const selectCartItems = createSelector(
+  //memoized selector
+  [selectCart], //1st arg-array of input selector
+  (cart) => cart.cartItems //func which returns the value we want out of this selector, parameter is output of input selector in the order they were writtem
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems], //can also take other selector apart from i/p selector
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
