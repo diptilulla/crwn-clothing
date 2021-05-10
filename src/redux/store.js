a6a3f0d47cdb290d5798901c0ff374fcdfc82b4e
diptilulla <60360333+diptilulla@users.noreply.github.com>
2021-05-10 14:54:07 +0530

    removing redux-logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,11 @@ import rootReducer from './root-reducer';
 
 //middlewares is going to be in array form
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger); //we want logger in dev only not in production, create-react-app sets an env variable, accessed thru process.env NODE_ENV is a property on process env file either be development production or test, npm start sets NODE_ENV variable to development npm build(with heroku) sets to production
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares)) //this will spread all methods and values in the array logger into this function as individual arguments
 
